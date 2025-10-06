a865286d3c415aeba321fe73bd12c246634886ba
Hugo <boyer.hugo974@gmail.com>
2025-10-07 01:12:43 +0200

    ajout du delete dans la todo

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
 const addbtn = document.getElementById("addBtn")
 const taskList = document.querySelector(".taskList")
-
+const taskOption = document.querySelector(".taskOption")
 
 
 addbtn.addEventListener("click", () => {
     const taskText = prompt("Quel est le nom de la tâche ?")
      if(taskText && taskText.trim() !== "") {
         addTask(taskText)
-     }
-    
+     }  
+})
+
+taskList.addEventListener("click",() => {
+    const maDiv = document.createElement("div")
+    maDiv.classList.add("edit")
+    maDiv.setAttribute("contenteditable", "true");
+    taskOption.appendChild(maDiv)
 })
 
 
 function addTask(task) {
+    // creer <div>
+    const div = document.createElement("div");
+    div.classList.add("task-item"); // optionnel pour le style
+
     // creer <li>
     const li = document.createElement("li");
     li.textContent = task;
 
-      li.addEventListener("click", () => {
-      li.classList.toggle("done");
-  });
+    const deleteDiv = document.createElement("div")
+        deleteDiv.textContent = "X"
+        deleteDiv.classList.add("delete"); // optionnel pour le style
+        
+    deleteDiv.addEventListener("click", () => {
+        div.remove()
+    })
 
-  taskList.appendChild(li)
+    
+    div.appendChild(li)
+    div.appendChild(deleteDiv)
+    taskList.appendChild(div)
+}
+
+function DeleteTask() {
+
+
+
 }
