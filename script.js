31dfdaf36a542b9c0abca64ad785b53ab3640d56
Hugo <boyer.hugo974@gmail.com>
2025-10-07 02:22:42 +0200

    feat: JS ajout de la zone edit selon la tache

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const addbtn = document.getElementById("addBtn")
 const taskList = document.querySelector(".taskList")
 const taskOption = document.querySelector(".taskOption")
-
+const todoContainer = document.getElementById("todo-container")
+let selectedTask = null
+const maDiv = document.createElement("div")
+maDiv.classList.add("edit")
+maDiv.setAttribute("contenteditable", "true");  
 
 addbtn.addEventListener("click", () => {
     const taskText = prompt("Quel est le nom de la tâche ?")
@@ -10,12 +14,7 @@ addbtn.addEventListener("click", () => {
      }  
 })
 
-taskList.addEventListener("click",() => {
-    const maDiv = document.createElement("div")
-    maDiv.classList.add("edit")
-    maDiv.setAttribute("contenteditable", "true");
-    taskOption.appendChild(maDiv)
-})
+
 
 
 function addTask(task) {
@@ -23,26 +22,46 @@ function addTask(task) {
     const div = document.createElement("div");
     div.classList.add("task-item"); // optionnel pour le style
 
-    // creer <li>
+    // creer <li pour l'affichage des tache>
     const li = document.createElement("li");
     li.textContent = task;
+    li.dataset.content = ""; // stocke le contenu éditable
+    
+    li.addEventListener("click",() => {
+    if(selectedTask) {
+        selectedTask.dataset.content = maDiv.innerHTML;
+       
+    } 
+    selectedTask = li 
+    maDiv.innerHTML = li.dataset.content    
+    maDiv.focus();
+})
+                       
 
+    //creer <div pour supprimer>
     const deleteDiv = document.createElement("div")
-        deleteDiv.textContent = "X"
-        deleteDiv.classList.add("delete"); // optionnel pour le style
+    deleteDiv.textContent = "X"
+    deleteDiv.classList.add("delete"); // optionnel pour le style
         
     deleteDiv.addEventListener("click", () => {
-        div.remove()
+        DeleteTask(div, maDiv)
     })
 
-    
+    taskOption.appendChild(maDiv)
     div.appendChild(li)
     div.appendChild(deleteDiv)
     taskList.appendChild(div)
 }
 
-function DeleteTask() {
 
 
-
+function DeleteTask(taskElement, blockElement) {
+    taskElement.remove()
+    blockElement.remove()
 }
+
+maDiv.addEventListener("input", () => {
+    if (selectedTask) {
+        selectedTask.dataset.content = maDiv.innerHTML;
+    }
+});
